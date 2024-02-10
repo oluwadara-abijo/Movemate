f2e0a5b9d2a17158d42e3401eb82795c39b8f6c0
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2024-02-10 15:33:28 +0100

    feat: create packaging component

--- app/src/main/java/com/dara/movemate/ui/composables/MovemateText.kt
+++ app/src/main/java/com/dara/movemate/ui/composables/MovemateText.kt
@@ -6,20 +6,38 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.dara.movemate.ui.theme.navy_blue
 
 @Composable
 fun LabelText(
     @StringRes textId: Int,
     modifier: Modifier = Modifier,
-    paddingValues: PaddingValues
+    paddingValues: PaddingValues = PaddingValues(),
+    fontWeight: FontWeight = FontWeight.Bold
 ) {
     Text(
         modifier = modifier.padding(paddingValues),
         text = stringResource(textId),
         fontSize = 22.sp,
-        fontWeight = FontWeight.Bold
+        fontWeight = fontWeight,
+        color = navy_blue
+    )
+}
+
+@Composable
+fun SecondaryText(
+    @StringRes textId: Int,
+    modifier: Modifier = Modifier,
+    paddingValues: PaddingValues = PaddingValues()
+) {
+    Text(
+        modifier = modifier.padding(paddingValues),
+        text = stringResource(textId),
+        color = Color.Gray,
+        fontSize = 16.sp
     )
 }
--- app/src/main/java/com/dara/movemate/ui/composables/calculate/CalculateScreen.kt
+++ app/src/main/java/com/dara/movemate/ui/composables/calculate/CalculateScreen.kt
@@ -24,6 +24,7 @@ fun CalculateScreen(
                 )
             }
             item { DestinationCard() }
+            item { PackagingComponent() }
         }
 
     }
